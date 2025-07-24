16e544c4bef3beaab0019fe3b7f9fdbc1b164d32
unknown <nezastsanjaya1@gmail.com>
2025-07-24 13:42:08 +0700

    Fix : Perbaikan pada metric dashboard jika data kosong

--- resources/views/partials/metric-group/metric-group-01.blade.php
+++ resources/views/partials/metric-group/metric-group-01.blade.php
@@ -1,13 +1,28 @@
 <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-6">
     <!-- Total Pendapatan Pemesanan per-Bulan -->
-    <x-box.metric-item title="Total Pemesanan (per-Bulan)" count="{{ 'Rp. ' . number_format($countTotalReservationPerMonth[date('m')], 0, ',', '.') }}" showPercent="true" percent="{{ $percentTotalReservationPerMonth }}" />
+    <x-box.metric-item 
+        title="Total Pemesanan (per-Bulan)" 
+        count="{{ 'Rp. ' . number_format($countTotalReservationPerMonth[date('m')] ?? 0, 0, ',', '.') }}" 
+        showPercent="true" 
+        percent="{{ $percentTotalReservationPerMonth ?? 0 }}" 
+    />
 
     <!-- Total Pendapatan Pemesanan Semua -->
-    <x-box.metric-item title="Total Pemesanan (Semua)" count="{{ 'Rp. ' . number_format($countTotalReservation, 0, ',', '.') }}" />
+    <x-box.metric-item 
+        title="Total Pemesanan (Semua)" 
+        count="{{ 'Rp. ' . number_format($countTotalReservation ?? 0, 0, ',', '.') }}" 
+    />
 
     <!-- Total Produk -->
-    <x-box.metric-item title="Total Produk" count="{{ $countProducts ?? 0 }}" />
+    <x-box.metric-item 
+        title="Total Produk" 
+        count="{{ $countProducts ?? 0 }}" 
+    />
 
     <!-- Total Pemesanan -->
-    <x-box.metric-item title="Total Pemesanan" count="{{ $countReservations ?? 0 }}" />
+    <x-box.metric-item 
+        title="Total Pemesanan" 
+        count="{{ $countReservations ?? 0 }}" 
+    />
 </div>
+
