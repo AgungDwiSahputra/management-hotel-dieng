cbb1f184d8b1ef7b0446ae9b4ea3cda0cb6fa491
unknown <nezastsanjaya1@gmail.com>
2025-07-24 23:57:59 +0700

    Fix : Perbaikan pada header dan isi table tidak sinkron pada halmana katersediaan

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -13,6 +13,16 @@ class CalendarController extends Controller
     {
         $products = getAllProducts() ?? [];
 
+        $products = array_map(function ($product) {
+            return [
+                'id' => $product['id'],
+                'name' => $product['name'],
+                'unit' => $product['unit'],
+                'harga_weekday' => $product['harga_weekday'],
+                'harga_weekend' => $product['harga_weekend'],
+            ];
+        }, $products);
+
         return view('calendar.index', [
             'title' => 'Ketersediaan',
             'description' => 'Halaman untuk mengelola Tanggal Reservasi',
