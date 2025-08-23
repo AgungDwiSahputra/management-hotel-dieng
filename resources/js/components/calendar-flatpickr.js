20c7fb6eefcc6320cc1f4d1c663967365bc13d99
unknown <nezastsanjaya1@gmail.com>
2025-08-23 19:12:47 +0700

    Fix : Perbaikan pada JS untuk handle elemnt tidak ditemukan

--- resources/js/components/calendar-flatpickr.js
+++ resources/js/components/calendar-flatpickr.js
@@ -190,8 +190,12 @@ class VillaCalendar {
     async renderEventsForDate(date) {
         // menetapkan nilai atribut ke bidang input
         const [year, month, day] = date.split("-");
-        this.elements.startDateInput.value = `${month}/${day}/${year}`;
-        this.elements.endDateInput.value = `${month}/${day}/${year}`;
+        const startDateInput = this.elements.startDateInput;
+        const endDateInput = this.elements.endDateInput;
+        if (startDateInput && endDateInput) {
+            startDateInput.value = `${month}/${day}/${year}`;
+            endDateInput.value = `${month}/${day}/${year}`;
+        }
 
         // mengambil data reservasi berdasarkan tanggal yang dipilih
         const reservations = await this.getReservationByDate(
